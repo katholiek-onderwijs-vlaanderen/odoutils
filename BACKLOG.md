30e9d6818db402be9498e61e8da1898455e2a8b0
Dimitry D'hondt <dimitry_dhondt@yahoo.com>
2022-11-10 15:11:24 +0100

    clean up list of errors.

--- BACKLOG.md
+++ BACKLOG.md
@@ -1,1 +1,11 @@
-* Add parameter/config option to allow specifying what module(s) to test.
+TO DO:
+
+* Test fresh install on ubuntu.
+* Add parameter/configuration mechanism to allow specifying the version of postgres to use.
+* Add parameter/configuration mechanism to allow specifying the version of odoo to use.
+* Log output starting at the position of the first FAILED test.
+
+DONE:
+
+* Add parameter/configuration mechanism to allow specifying what module(s) to test.
+* Clean up list of failed tests.
--- run-tests.sh
+++ run-tests.sh
@@ -8,11 +8,10 @@ DOCKER_PG=om-hospital-test-pg
 trap ctrl_c INT
 function remove_temp_files {
 	# Clean up temporary files
-	rm $ERRORS 2&>1 >>$TRACE
-	rm $LOG 2&>1 >>$TRACE
-	if [ -f $TRACE ]
-	then
-		rm $TRACE 2&>1 >/dev/null
+	rm $ERRORS 2 &>1 >>$TRACE
+	rm $LOG 2 &>1 >>$TRACE
+	if [ -f $TRACE ]; then
+		rm $TRACE 2 &>1 >/dev/null
 	fi
 }
 
@@ -28,7 +27,7 @@ function ctrl_c() {
 
 function please_install {
 	echo "This script requires the <$1> command. Please install it."
-	echo 
+	echo
 	echo "On Ubuntu for example:"
 	echo
 	echo "$ sudo apt-get install $2"
@@ -41,15 +40,14 @@ remove_temp_files
 # Check if all dependencies are installed..
 command -v figlet >>$TRACE || please_install figlet figlet
 command -v docker >>$TRACE || please_install docker docker.io
-command -v tput >>$TRACE || please_install tput tput 
+command -v tput >>$TRACE || please_install tput tput
 command -v inotifywait >>$TRACE || please_install inotifywait inotify-tools
 
 # If we are running on WSL, check that the docker command
 # is telling us to start the docker engine via the UI...
-docker 2&>1 >/tmp/docker.log
+docker 2 &>1 >/tmp/docker.log
 not_found=$(cat /tmp/docker.log | grep "could not be found" | wc -l)
-if [ $not_found -ne 0 ]
-then
+if [ $not_found -ne 0 ]; then
 	cat /tmp/docker.log
 	echo
 	echo "***************************************************************************"
@@ -58,18 +56,23 @@ then
 	exit 1
 fi
 
+if [ $# -ne 1 ]; then
+	echo "Usage:"
+	echo "$ ./run-tests.sh [module name]"
+	exit 1
+fi
+MODULE=$1
 
+echo "Checking if the postgres docker exists." >>$TRACE
 found_docker_pg=$(docker ps -a | grep $DOCKER_PG | wc -l)
-if [ $found_docker_pg -eq 0 ]
-then
+if [ $found_docker_pg -eq 0 ]; then
 	echo "Creating a postgres:10 server." >>$TRACE
 	docker create -p 5433:5432 -e POSTGRES_USER=odoo -e POSTGRES_PASSWORD=odoo -e POSTGRES_DB=postgres --name $DOCKER_PG postgres:10 >>$TRACE
 fi
 
-
+echo "Checking if the odoo docker exists." >>$TRACE
 found_docker_odoo=$(docker ps -a | grep $DOCKER_ODOO | wc -l)
-if [ $found_docker_odoo -eq 0 ]
-then
+if [ $found_docker_odoo -eq 0 ]; then
 	echo "Creating the odoo server to run the tests." >>$TRACE
 	docker create -v ~/prj:/mnt/extra-addons -p 8071:8069 --name $DOCKER_ODOO --link $DOCKER_PG:db odoo:15 -d odoo -u om_hospital -i om_hospital --stop-after-init --test-tags /om_hospital >>$TRACE
 fi
@@ -78,9 +81,8 @@ fi
 echo "Starting the postgres server." >>$TRACE
 docker start $DOCKER_PG >>$TRACE
 
-while true
-do
-	hash=$(find . -type f -exec ls -l {} + | sort | md5sum)
+while true; do
+	hash=$(find $MODULE -type f -exec ls -l {} + | sort | md5sum)
 	echo "Calculated hash for the folder where we are running AT START OF CYCLE: $hash" >>$TRACE
 
 	timestamp=$(date --rfc-3339=seconds | sed "s/ /T/")
@@ -89,11 +91,11 @@ do
 	echo "(Re)starting the odoo server to run the test suite." >>$TRACE
 	docker restart $DOCKER_ODOO >>$TRACE
 	docker logs -f --since $timestamp $DOCKER_ODOO 2>$LOG
+	echo "Finished running the tests..." >>$TRACE
 
 	echo "Server finised running the odoo test suite." >>$TRACE
 	cat $LOG | grep "ERROR.*test.*FAIL:" >$ERRORS
-	if [ -s $ERRORS ]
-	then
+	if [ -s $ERRORS ]; then
 		echo -n "$(tput bold)$(tput setaf 7)$(tput setab 1)"
 		clear
 
@@ -102,17 +104,18 @@ do
 		echo
 
 		echo "Displaying list of failed tests." >>$TRACE
-		cat $ERRORS | cut -c -$(tput cols)
-		echo "---"
+		echo "$(tput smso)These tests failed:$(tput rmso)"
+		cat $ERRORS | sed 's/.*FAIL: //g' | cut -c -$(tput cols)
 		echo
 
 		error_count=$(cat $ERRORS | wc -l)
 		echo "Counted $error_count errors in the odoo logs." >>$TRACE
-		
-		lines=`expr $(tput lines) - 10 - $error_count`
+
+		lines=$(expr $(tput lines) - 11 - $error_count)
 		echo "Number of lines to tail on the rest of the screen: $lines" >>$TRACE
 
 		echo "Showing tail of odoo logs on screen." >>$TRACE
+		echo "$(tput smso)Logs of the odoo server:$(tput rmso)"
 		tail -n $lines $LOG | cut -c -$(tput cols)
 	else
 		echo -n "$(tput bold)$(tput setaf 7)$(tput setab 2)"
@@ -123,17 +126,16 @@ do
 		echo
 
 		echo "Number of lines to tail on the rest of the screen: $lines" >>$TRACE
-		lines=`expr $(tput lines) - 8`
+		lines=$(expr $(tput lines) - 8)
 
 		echo "Showing tail of odoo log on screen." >>$TRACE
 		tail -n $lines $LOG | cut -c -$(tput cols)
 	fi
 
-	hash2=$(find . -type f -exec ls -l {} + | sort | md5sum)
+	hash2=$(find $MODULE -type f -exec ls -l {} + | sort | md5sum)
 	echo "Calculated hash of the folder where we are running AT END OF CYCLE: $hash2" >>$TRACE
-	if [ "$hash" = "$hash2" ]
-	then
+	if [ "$hash" = "$hash2" ]; then
 		echo "Waiting for changes on the filesystem." >>$TRACE
-		inotifywait -r -q . 2>&1 >>$TRACE
+		inotifywait -r -q $MODULE 2>&1 >>$TRACE
 	fi
 done
